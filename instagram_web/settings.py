8ee5e06b9f942fcc59725120974bfe0fc7779717
lovewjdvud <76860314+lovewjdvud@users.noreply.github.com>
2024-06-25 15:18:57 +0900

    UI : navigation bar

--- instagram_web/settings.py
+++ instagram_web/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
+    "content",
+    "instagram_web",
+    "user",
 ]
 
 MIDDLEWARE = [
